a004c3b6ea6136580080474232f76af40a5675e9
free <18602871035@163.com>
2012-06-12 10:49:56 +0800

    add filterByIndexHash(), and moditifing

--- src/model.lua
+++ src/model.lua
@@ -1,5 +1,6 @@
 module(..., package.seeall)
 local socket = require 'socket'
+local mih = require 'bamboo.model-indexhash'
 
 local tinsert, tremove = table.insert, table.remove
 local format = string.format
@@ -144,13 +145,13 @@ local function getIndexKey(self)
 	return getClassName(self) + ':__index'
 end
 
-local function getFieldIndexKey(self, field)
-	return getClassName(self) .. ":" .. field.. ':__index'
-end
+--local function getFieldIndexKey(self, field)
+--	return getClassName(self) .. ":" .. field.. ':__index'
+--end
 
-local function getSetIndexKey(self, field, value)
-	return getClassName(self) .. ":" .. field..":".. value .. ':__index'
-end
+--local function getSetIndexKey(self, field, value)
+--	return getClassName(self) .. ":" .. field..":".. value .. ':__index'
+--end
 
 local function getClassIdPattern(self)
 	return getClassName(self) + self.id
@@ -2178,6 +2179,8 @@ Model = Object:extend {
 			db:zadd(index_key, self.id, self[indexfd])
 			-- update object hash store key
 			db:hmset(model_key, unpack(store_kv))
+            
+            self:indexHash(true);--create hash index
 			end)
 		else
 			-- update case
@@ -2188,6 +2191,8 @@ Model = Object:extend {
 
 			local options = { watch = {index_key}, cas = true, retry = 2 }
 			replies = db:transaction(options, function(db)
+            self:indexHash(false);--update hash index
+
 			local score = db:zscore(index_key, self[indexfd])
 			assert(score == self.id or score == nil, "[Error] save duplicate to an unique limited field, aborted!")
 			
@@ -2919,77 +2924,40 @@ Model = Object:extend {
 
     -- params :
     -- if field == nil then index all field in __index 
-    indexHash = function(self,field)
+    indexHash = function(self,newIndex,field)
 		I_AM_INSTANCE(self)
-		assert(self.id, ("[Error] Must have id field!"))
         if field then 
 		    checkType(field, 'string')
         end
 
-
-       local function indexFieldStrRemove(self, field)
-            local indexKey = getFieldIndexKey(self,field);
-            local id = db:hget(indexKey, self[field]); 
-            if id==nil then 
-                --do nothing
-            elseif tonumber(id) then--this field only has a id
-                if tonumber(id) == tonumber(self.id) then 
-                    db:hdel(indexKey, self[field]);
+        -- start index
+        if newIndex then 
+            if field then -- index for field 
+                indexType = self.__fields[field].indexType;
+                if indexType then 
+                    mih.indexField(self, field, indexType, nil)
                 end
-            else --this field has a id set 
-                local indexSetKey = id;
-                db:srem(indexSetKey, self.id);
-
-                local num = db:scard(indexSetKey) ;
-                if num == 1 then 
-                    local ids = db:smembers(indexSetKey);
-                    db:del(indexSetKey);
-                    db:hset(indexKey,self[field],ids[1]);
+            else -- index for object
+                for field, def in pairs(self.__fields) do
+                    if def.indexType then
+                        mih.indexField(self, field, def.indexType, nil);
+                    end
                 end
             end
-        end
-
-        local function indexFieldStr(self,field) --add the new
-            local indexKey = getFieldIndexKey(self,field);
-            local id = db:hget(indexKey, self[field]); 
-            if id==nil then 
-                db:hset(indexKey, self[field], self.id);
-            elseif tonumber(id) then--this field already has a id
-                local indexSetKey = getSetIndexKey(self, field, self[field]);
-                db:sadd(indexSetKey, id);
-                db:sadd(indexSetKey, self.id);
-                db:hset(indexKey, self[field], indexSetKey);
-            else -- this field has a id set, the id is the set name  
-                db:sadd(id, self.id);
-            end
-        end
-
-        local function indexField(self, field, indexType, oldObj)
-            indexType = indexType or self.__fields[field].indexType;
-            
-            local value = self[field];
-            if oldObj.__fields[field] == self.__fields[field] then
-                return;
-            end
-            
-            if indexType == 'number' then
-                local indexKey = getFieldIndexKey(self,field);
-                db:zadd(indexKey, value, self.id);
-            elseif indexType == 'string' then
-                indexFieldStrRemove(oldObj,field);--remove the old
-                indexFieldStr(self,field); --add the new
-            else
-                
-            end
-        end
-
-        local oldObj = self:getClass():getById(self.id);
-
-        if field then -- index for field 
-            indexField(self, field, nil, oldObj)
-        else -- index for object
-            for field, def in pairs(self.__fields) do
-                indexField(self, field, def.indexType, oldObj);
+        else
+            local oldObj = self:getClass():getById(self.id);
+    
+            if field then -- index for field 
+                indexType = self.__fields[field].indexType;
+                if indexType then 
+                    mih.indexField(self, field, nil, oldObj)
+                end
+            else -- index for object
+                for field, def in pairs(self.__fields) do
+                    if def.indexType then
+                        mih.indexField(self, field, def.indexType, oldObj);
+                    end
+                end
             end
         end
     end;
@@ -2998,14 +2966,13 @@ Model = Object:extend {
     getIdsByIndexHash = function(self,field,value)
 		I_AM_CLASS(self)
             
-        local indexKey = getFieldIndexKey(self,field);
+        local indexKey = mih.getFieldIndexKey(self,field);
         if self.__fields[field].indexType== 'number' then 
             value = tonumber(value);
             return db:zrangebyscore(indexKey,value,value)
         elseif self.__fields[field].indexType == 'string' then 
-            local indexKey = getFieldIndexKey(self,field);
+            local indexKey = mih.getFieldIndexKey(self,field);
             local id = db:hget(indexKey, value); 
-            print(id,indexKey,field,value);
             if id == nil then 
                 return List();
             elseif tonumber(id) then
@@ -3020,7 +2987,192 @@ Model = Object:extend {
         else
             return List();
         end
-    end
+    end;--]]
+
+	--- fitler some instances belong to this model
+	-- @param query_args: query arguments in a table
+	-- @param start: specify which index to start slice, note: this is the position after filtering 
+	-- @param stop: specify the end of slice
+	-- @param is_rev: specify the direction of the search result, 'rev'
+	-- @return: query_set, an object list (query set)
+	-- @note: this function can be called by class object and query set
+	filterByIndexHash = function (self, query_args, start, stop, is_rev, is_get)
+		I_AM_CLASS_OR_QUERY_SET(self)
+		assert(type(query_args) == 'table' or type(query_args) == 'function', '[Error] the query_args passed to filter must be table or function.')
+		if start then assert(type(start) == 'number', '[Error] @filter - start must be number.') end
+		if stop then assert(type(stop) == 'number', '[Error] @filter - stop must be number.') end
+		if is_rev then assert(type(is_rev) == 'string', '[Error] @filter - is_rev must be string.') end
+		
+		local is_query_set = false
+		if isQuerySet(self) then is_query_set = true end
+		local is_query_table = (type(query_args) == 'table')
+		local logic = 'and'
+		
+		local query_str_iden
+		local is_using_rule_index = isUsingRuleIndex(self)
+		if is_using_rule_index then
+			if type(query_args) == 'function' then
+				collectRuleFunctionUpvalues(query_args)
+			                                   
+			end
+			-- make query identification string
+			query_str_iden = compressQueryArgs(query_args)
+
+			-- check index
+			-- XXX: Only support class now, don't support query set, maybe query set doesn't need this feature
+			local id_list = getIndexFromManager(self, query_str_iden)
+			if #id_list > 0 then
+				if is_get == 'get' then
+					id_list = (is_rev == 'rev') and List{id_list[#id_list]} or List{id_list[1]}
+				else	
+					-- now id_list is a list containing all id of instances fit to this query_args rule, so need to slice
+					id_list = id_list:slice(start, stop, is_rev)
+				end
+				
+				-- if have this list, return objects directly
+				if #id_list > 0 then
+					return getFromRedisPipeline(self, id_list)
+				end
+			end
+			-- else go ahead
+		end
+		
+		if is_query_table then
+			if query_args and query_args['id'] then
+				-- remove 'id' query argument
+				print("[Warning] Filter doesn't support search by id.")
+				query_args['id'] = nil 
+			end
+
+			-- if query table is empty, return slice instances
+			if isFalse(query_args) then 
+				local start = start or 1
+				local stop = stop or -1
+				local nums = self:numbers()
+				return self:slice(start, stop, is_rev)
+			end
+
+			-- normalize the 'and' and 'or' logic
+			if query_args[1] then
+				assert(query_args[1] == 'or' or query_args[1] == 'and', 
+					"[Error] The logic should be 'and' or 'or', rather than: " .. tostring(query_args[1]))
+				if query_args[1] == 'or' then
+					logic = 'or'
+				end
+				query_args[1] = nil
+			end
+		end
+		
+		local all_ids
+		if is_query_set then
+			-- if self is query set, we think of all_ids as object list, rather than id string list
+			all_ids = self
+		else
+			-- all_ids is id string list
+			all_ids = self:allIds()
+		end
+		-- nothing in id list, return empty table
+		if #all_ids == 0 then return List() end
+
+		
+		-- create a query set
+		local query_set = QuerySet()
+		local logic_choice = (logic == 'and')
+		local partially_got = false
+
+		-- walkcheck can process full object and partial object
+		local walkcheck = function (objs)
+			for i = 1, #all_ids do
+				local obj = objs[i]
+				--DEBUG(obj)
+				-- check the object's legalery, only act on valid object
+				--if isValidInstance(obj) then
+				local flag = checkLogicRelation(self, obj, query_args, logic_choice)
+				
+				-- if walk to this line, means find one 
+				if flag then
+					tinsert(query_set, obj)
+				end
+				--end
+			end
+		end
+		
+		--DEBUG('all_ids', all_ids)
+		if is_query_set then
+			local objs = all_ids
+			-- objs are already integrated instances
+			walkcheck(objs)			
+		else
+			-- make partially get value containing 'id' default
+			local qfs = {'id'}
+			if is_query_table then
+				for k, _ in pairs(query_args) do
+					tinsert(qfs, k)
+				end
+			else
+				-- use precollected fields
+				-- if model has set '__use_rule_index' manually, collect all fields to index
+				-- if not set '__use_rule_index' manually, collect fields with 'index=true' in their field description table
+				-- if not set '__use_rule_index' manually, and not set 'index=true' in any field, collect NOTHING
+				--DEBUG('__rule_index_fields', self.__rule_index_fields)
+				for _, k in ipairs(self.__rule_index_fields) do
+					tinsert(qfs, k)
+				end
+			end
+			table.sort(qfs)
+			local objs
+			--DEBUG(qfs)
+			-- == 1, means only have 'id', collect nothing on fields 
+			if #qfs == 1 then
+				--DEBUG('Enter full pipeline branch')
+				-- collect nothing, use 'hgetall' to retrieve, partially_got is false
+				objs = getFromRedisPipeline(self, all_ids)
+			else
+				--DEBUG('Enter partial pipeline branch')
+				-- use hmget to retrieve, now the objs are partial objects
+				objs = getPartialFromRedisPipeline(self, all_ids, qfs)
+				partially_got = true
+			end
+			walkcheck(objs)			
+		end
+		
+		-- here, _t_query_set is the all instance fit to query_args now
+		local _t_query_set = query_set
+		if #query_set == 0 then return query_set end
+		
+		if is_get == 'get' then
+			query_set = (is_rev == 'rev') and List {_t_query_set[#_t_query_set]} or List {_t_query_set[1]}
+		else	
+			-- now id_list is a list containing all id of instances fit to this query_args rule, so need to slice
+			query_set = _t_query_set:slice(start, stop, is_rev)
+		end
+
+		-- if self is query set, its' element is always integrated
+		-- if call by class
+		if not is_query_set and #_t_query_set > 0 then
+			-- retrieve all objects' id
+			local id_list = {}
+			for _, v in ipairs(_t_query_set) do
+				tinsert(id_list, v.id)
+			end
+			-- add to index, here, we index all instances fit to query_args, rather than results applied extra limitation conditions
+			if is_using_rule_index then
+				addIndexToManager(self, query_str_iden, id_list)
+			end
+			
+			-- if partially got previously, need to get the integrated objects now
+			if partially_got then
+				id_list = {}
+				-- retrieve needed objects' id
+				for _, v in ipairs(query_set) do
+					tinsert(id_list, v.id)
+				end
+				query_set = getFromRedisPipeline(self, id_list)
+			end
+		end
+
+		return query_set
+	end;
 }
 
 local QuerySetMeta = setProto({__spectype='QuerySet'}, Model)
--- tests/index.lua
+++ tests/index.lua
@@ -28,7 +28,6 @@ local Test = Model:extend{
 	__fields = {
 		['name'] = { indexType="string", required=true },
 		['score'] = { indexType="number", required=true },
-		['create_time'] = {},
 	};
 
     init = function (self,t)
@@ -36,7 +35,7 @@ local Test = Model:extend{
 
         self.name = t.name;
         self.score = t.score;
-        self.create_time = os.time();
+        self.created_time = os.time();
 
         return self
     end
@@ -61,62 +60,58 @@ function testMain()
     local test5 = Test({name = "xxxx1",score = 2.1})
     local test6 = Test({name = "xxxx2",score = 3.0})
     
-    test1:save(); print("test1",test1.id);
-    test2:save(); print("test2",test2.id);
-    test3:save(); print("test3",test3.id);
-    test4:save(); print("test4",test4.id);
-    test5:save(); print("test5",test5.id);
-    test6:save(); print("test6",test6.id);
-   
     print("TEST INDEX");
-    test1:indexHash();
+    test1:save(); print("test1",test1.id);
     assert(tonumber(db:hget("Test:name:__index",test1.name)) == tonumber(test1.id), "the hash vaule must be the id"); 
     assert(tonumber(db:zscore("Test:score:__index",test1.id)) == tonumber(test1.score), "the score vaule must be the id"); 
-    
-    test2:indexHash();
-    assert(tonumber(db:hget("Test:name:__index",test1.name)) == "Test:name:xxxx:__index", 
+
+    test2:save(); print("test2",test2.id);
+    assert((db:hget("Test:name:__index",test1.name)) == "Test:name:xxxx:__index", 
                     "the hash vaule must be the key of the Set of objects that the name value be 'xxxx'"); 
-    assert(db:sismember("Test:name:xxxx:__index",test1.id)==1 , 
+    assert(db:sismember("Test:name:xxxx:__index",test1.id) , 
                     "the id must be in the Set of objects that the name value be 'xxxx'"); 
-    assert(db:sismember("Test:name:xxxx:__index",test2.id)==1 , 
+    assert(db:sismember("Test:name:xxxx:__index",test2.id) , 
                     "the id must be in the Set of objects that the name value be 'xxxx'"); 
     assert(db:scard("Test:name:xxxx:__index")==2 , 
                     "the members number in the Set now must be 2"); 
-    assert(tonumber(db:zscore("Test:score:__index",test1.id)) == tonumber(test2.score), 
+    assert(tonumber(db:zscore("Test:score:__index",test1.id)) == tonumber(test1.score), 
                     "the score vaule must be the id"); 
     assert(tonumber(db:zscore("Test:score:__index",test2.id)) == tonumber(test2.score), 
                     "the score vaule must be the id"); 
 
-    test3:indexHash();
-    assert(tonumber(db:hget("Test:name:__index",test1.name)) == "Test:name:xxxx:__index", 
+    test3:save(); print("test3",test3.id);
+    assert((db:hget("Test:name:__index",test1.name)) == "Test:name:xxxx:__index", 
                     "the hash vaule must be the key of the Set of objects that the name value be 'xxxx'"); 
-    assert(db:sismember("Test:name:xxxx:__index",test1.id)==1 , 
+    assert(db:sismember("Test:name:xxxx:__index",test1.id), 
                     "the id must be in the Set of objects that the name value be 'xxxx'"); 
-    assert(db:sismember("Test:name:xxxx:__index",test2.id)==1 , 
+    assert(db:sismember("Test:name:xxxx:__index",test2.id) , 
                     "the id must be in the Set of objects that the name value be 'xxxx'"); 
-    assert(db:sismember("Test:name:xxxx:__index",test3.id)==1 , 
+    assert(db:sismember("Test:name:xxxx:__index",test3.id) , 
                     "the id must be in the Set of objects that the name value be 'xxxx'"); 
     assert(db:scard("Test:name:xxxx:__index")==3 , 
                     "the members number in the Set now must be 3"); 
     assert(tonumber(db:zscore("Test:score:__index",test2.id)) == tonumber(test2.score), 
                     "the score vaule must be the id"); 
 
-    test4:indexHash();
-    test5:indexHash();
-    test6:indexHash();
-
+    test4:save(); print("test4",test4.id);
+    test5:save(); print("test5",test5.id);
+    test6:save(); print("test6",test6.id);
+   
+    print("filter name xxxx2")
     local ids = Test:getIdsByIndexHash("name","xxxx2");
     for i,v in ipairs(ids) do
         local obj = Test:getById(v);
         print(obj.id, obj.name, obj.score);
     end
 
+    print("filter name xxxx1")
     local ids = Test:getIdsByIndexHash("name","xxxx1");
     for i,v in ipairs(ids) do
         local obj = Test:getById(v);
         print(obj.id, obj.name, obj.score);
     end
 
+    print("filter name xxxx")
     local ids = Test:getIdsByIndexHash("name","xxxx");
     for i,v in ipairs(ids) do
         local obj = Test:getById(v);
@@ -125,20 +120,25 @@ function testMain()
 
 
     test1.name = "xxxx2";
-    test1:indexHash();
     test1:save();
     test2.name = "xxxx2";
-    test2:indexHash();
     test2:save();
     test3.name = "xxxx2";
-    test3:indexHash();
     test3:save();
 
+    print("filter name xxxx")
     local ids = Test:getIdsByIndexHash("name","xxxx");
     for i,v in ipairs(ids) do
         local obj = Test:getById(v);
         print(obj.id, obj.name, obj.score);
     end
+    print("filter name xxxx1")
+    local ids = Test:getIdsByIndexHash("name","xxxx1");
+    for i,v in ipairs(ids) do
+        local obj = Test:getById(v);
+        print(obj.id, obj.name, obj.score);
+    end
+    print("filter name xxxx2")
     local ids = Test:getIdsByIndexHash("name","xxxx2");
     for i,v in ipairs(ids) do
         local obj = Test:getById(v);
